58f10dc84c3728b7e80ca792adbb5d9ad9fa179f
rianfernandesno <zrpapz12@gmail.com>
2025-06-06 22:26:30 -0300

    "Interface Command"

---
+++ src/main/java/com/yuicottrill/discordbot/commands/Command.java
@@ -0,0 +1,8 @@
+package com.yuicottrill.discordbot.commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public interface Command {
+
+    void execute(MessageReceivedEvent event, String[] args);
+}
