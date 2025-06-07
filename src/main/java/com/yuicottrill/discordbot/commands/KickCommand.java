b60d8149e02ee71fc69063155d46737514acfa77
rianfernandesno <zrpapz12@gmail.com>
2025-06-06 23:02:34 -0300

    "New Command Kick "

---
+++ src/main/java/com/yuicottrill/discordbot/commands/KickCommand.java
@@ -0,0 +1,26 @@
+package com.yuicottrill.discordbot.commands;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Mentions;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class KickCommand implements Command{
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args) {
+        List<Member> mentionedMember = event.getMessage().getMentions().getMembers();
+
+        if(!mentionedMember.isEmpty()){
+            Member target = mentionedMember.get(0);
+
+            event.getGuild()
+                    .kick(target)
+                    .reason("kick test")
+                    .queue();
+        }
+
+    }
+}
