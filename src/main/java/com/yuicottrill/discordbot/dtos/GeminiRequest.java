1008063c4a7f227dbcd6fd356e7e7d2f79adaa3a
rianfernandesno <zrpapz12@gmail.com>
2025-06-07 17:45:36 -0300

    "DTO Gemini API"

---
+++ src/main/java/com/yuicottrill/discordbot/dtos/GeminiRequest.java
@@ -0,0 +1,40 @@
+package com.yuicottrill.discordbot.dtos;
+
+import java.util.List;
+
+public class GeminiRequest {
+
+    private List<Content> contents;
+
+    public List<Content> getContents() {
+        return contents;
+    }
+
+    public void setContents(List<Content> contents) {
+        this.contents = contents;
+    }
+
+    public static class Content{
+        private List<Part> parts;
+
+        public List<Part> getParts() {
+            return parts;
+        }
+
+        public void setParts(List<Part> parts) {
+            this.parts = parts;
+        }
+    }
+
+    public  static class Part{
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+    }
+}
