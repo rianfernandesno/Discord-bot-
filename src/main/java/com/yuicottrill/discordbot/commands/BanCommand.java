17eccbc20a32635b5a2fe9761ac8a59b53ee53cb
rianfernandesno <zrpapz12@gmail.com>
2025-06-06 23:20:19 -0300

    "new Ban Command"

---
+++ src/main/java/com/yuicottrill/discordbot/commands/BanCommand.java
@@ -0,0 +1,27 @@
+package com.yuicottrill.discordbot.commands;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class BanCommand implements Command{
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args) {
+
+        List<Member> members = event.getMessage().getMentions().getMembers();
+
+        if (!members.isEmpty()){
+
+            Member member = members.getFirst();
+
+            event.getGuild()
+                    .ban(member.getUser(), 7, TimeUnit.DAYS)
+                    .reason("Banido")
+                    .queue();
+
+        }
+    }
+}
