ed0181cbc5cd825190dc40cccd0796d2c1744cf2
rianfernandesno <zrpapz12@gmail.com>
2025-06-08 10:45:48 -0300

    "Unmuted command"

---
+++ src/main/java/com/yuicottrill/discordbot/commands/UnmutedCommand.java
@@ -0,0 +1,51 @@
+package com.yuicottrill.discordbot.commands;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class UnmutedCommand implements Command {
+
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args) {
+
+        List<Member> members = event.getMessage().getMentions().getMembers();
+
+        if (!members.isEmpty()){
+
+            if (!event.getMember().hasPermission(Permission.BAN_MEMBERS)){
+
+                event.getChannel().sendMessage("Você não tem permissão de desmutar usuários").queue();
+                throw new IllegalArgumentException("Não pode mutar usuários");
+            }
+
+            Member member = members.getFirst();
+            List<TextChannel> channels = event.getGuild().getTextChannels();
+
+            for (TextChannel channel : channels){
+
+                channel.upsertPermissionOverride(member)
+                        .grant(Permission.MESSAGE_SEND)
+                        .queue();
+            }
+
+            Role mutedRole = event.getGuild()
+                    .getRolesByName("Mutado!", true)
+                    .stream()
+                    .findFirst()
+                    .orElse(null);
+
+            event.getGuild()
+                            .removeRoleFromMember(member, mutedRole)
+                                    .queue();
+
+            event.getChannel().sendMessage("O usuário foi desmutado!").queue();
+        }
+    }
+}
