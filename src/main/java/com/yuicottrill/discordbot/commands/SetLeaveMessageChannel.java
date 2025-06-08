8ce457b4d8ffc63b854d28a0a1e01ec1d714bf1f
rianfernandesno <zrpapz12@gmail.com>
2025-06-08 19:54:56 -0300

    "fix: prevent users without permission from using the set commands "

--- src/main/java/com/yuicottrill/discordbot/commands/SetLeaveMessageChannel.java
+++ src/main/java/com/yuicottrill/discordbot/commands/SetLeaveMessageChannel.java
@@ -1,6 +1,7 @@
 package com.yuicottrill.discordbot.commands;
 
 import com.yuicottrill.discordbot.service.GuildDataService;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,16 @@ public class SetLeaveMessageChannel  implements Command{
 
     @Override
     public void execute(MessageReceivedEvent event, String[] args) {
+        if (!event.getMember().hasPermission(Permission.MANAGE_CHANNEL)) {
+            event.getChannel().sendMessage("Você não tem acesso a esse comando!").queue();
+            return; // Impede a continuação do método se não tiver permissão
+        }
+
+        if (args.length == 0) {
+            event.getChannel().sendMessage("Você precisa fornecer o ID da role.").queue();
+            return;
+        }
+
         String leaveMessage = args[0];
 
         service.saveLeaveChannel(event.getGuild(), leaveMessage);
